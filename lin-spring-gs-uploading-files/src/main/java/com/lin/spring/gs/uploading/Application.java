851b15b44214af2b770eaebf65dc4983a024d35b
langyan <langyan.lin@gmail.com>
2018-05-17 21:17:06 +0800

    'upload'

---
+++ lin-spring-gs-uploading-files/src/main/java/com/lin/spring/gs/uploading/Application.java
@@ -0,0 +1,27 @@
+package com.lin.spring.gs.uploading;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+
+import com.lin.spring.gs.uploading.storage.StorageProperties;
+import com.lin.spring.gs.uploading.storage.StorageService;
+
+@SpringBootApplication
+@EnableConfigurationProperties(StorageProperties.class)
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    CommandLineRunner init(StorageService storageService) {
+        return (args) -> {
+            storageService.deleteAll();
+            storageService.init();
+        };
+    }
+}
---
+++ lin-spring-gs-uploading-files/src/main/java/com/lin/spring/gs/uploading/storage/StorageException.java
@@ -0,0 +1,12 @@
+package com.lin.spring.gs.uploading.storage;
+
+public class StorageException extends RuntimeException {
+
+    public StorageException(String message) {
+        super(message);
+    }
+
+    public StorageException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
