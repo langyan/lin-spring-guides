22b39ad3fb8fbf059ba22c07124a9f59394951da
langyan <langyan.lin@gmail.com>
2018-05-18 23:08:34 +0800

    'security'

---
+++ lin-spring-gs-securing-web/src/main/java/com/lin/spring/gs/security/Application.java
@@ -0,0 +1,13 @@
+package com.lin.spring.gs.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) throws Throwable {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
---
+++ lin-spring-gs-securing-web/src/main/java/com/lin/spring/gs/security/WebSecurityConfig.java
@@ -0,0 +1,57 @@
+package com.lin.spring.gs.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+            .authorizeRequests()
+                .antMatchers("/", "/home").permitAll()
+                .anyRequest().authenticated()
+                .and()
+            .formLogin()
+                .loginPage("/login")
+                .permitAll()
+                .and()
+            .logout()
+                .permitAll();
+    }
+
+    @Bean
+    @Override
+    public UserDetailsService userDetailsService() {
+    	
+   	  UserDetails user = User.withUsername("user")
+   			      .password("{bcrypt}$2a$10$dXJ3SW6G7P50lGmMkkmwe.20cQQubK3.HZWzG3YB1tlRy.fqvM/BG")
+   			      .roles("USER")
+   			      .build();
+   	  
+//   	user.getPassword()
+//        UserDetails user =
+//             User.withDefaultPasswordEncoder()
+//                .username("user")
+//                .password("password")
+//                .roles("USER")
+//                .build();
+   	  
+ 	  PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+ 	  // outputs {bcrypt}$2a$10$dXJ3SW6G7P50lGmMkkmwe.20cQQubK3.HZWzG3YB1tlRy.fqvM/BG
+ 	  // remember the password that is printed out and use in the next step
+ 	  System.out.println(encoder.encode("password"));
+
+        return new InMemoryUserDetailsManager(user);
+    }
+}
